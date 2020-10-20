645f5e0b261d50de6b165b2fdbbd8ec3af4ec017
Ferid <kamouaferid@gmail.com>
2020-10-20 08:26:34 -0400

    fixing chat scroll

--- scripts/chat.js
+++ scripts/chat.js
@@ -56,8 +56,8 @@
         var listener = function(e) {
             e.preventDefault();
         }
-        document.addEventListener('touchmove', listener, {passive:false, once:true});
-        body.style.backgroundColor = 'green';
+        document.addEventListener('touchmove', listener, {passive:false, once:false});
+        body.style.backgroundColor = 'orange';
     }
 
  
