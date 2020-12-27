0bb8a1ad9f0c819699a0fa5e97514037cf3232bb
ferid <kamouaferid@gmail.com>
2020-12-27 07:57:31 -0500

    link hover transition

--- scripts/hover.js
+++ scripts/hover.js
@@ -1,1 +1,20 @@
-const link = document.getElementsByTagName('a');
+const links = document.getElementsByTagName('a');
+const temp = document.querySelector('.projects__name');
+
+
+
+const removeOpacity = (e) => {
+    e.target.classList.remove('a--opacity');
+};
+
+
+
+for (const v of links) {
+    v.addEventListener('transitionend', (e) => {
+        if(e.target.addEventListener('mouseout', removeOpacity(e))) {
+            e.target.removeEventListener('mouseout', removeOpacity(e));
+        }
+
+    });
+    v.addEventListener('mouseover', (e) => e.target.classList.add('a--opacity'));
+}
